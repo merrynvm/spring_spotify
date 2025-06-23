5116e21beefee6d971011267b4d0c356265ac2d3
merry <rozzimario18@gmail.com>
2025-06-24 00:24:23 +0200

    created user entity with userType

---
+++ src/main/java/com/example/spotify/user/Suser.java
@@ -0,0 +1,71 @@
+package com.example.spotify.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+
+@Entity
+public class Suser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+    private Email email;
+    private String password;
+
+    private SuserType type;
+
+    private Suser(){}
+
+    private Suser(Long id, String nickname, Email email, String password, SuserType type){
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public SuserType getType() {
+        return type;
+    }
+
+    public void setType(SuserType type) {
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/com/example/spotify/user/SuserType.java
@@ -0,0 +1,5 @@
+package com.example.spotify.user;
+
+public enum SuserType {
+    ARTIST, USER
+}
